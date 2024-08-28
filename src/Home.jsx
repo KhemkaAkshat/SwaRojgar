79ffb742c71eca7291c74300d15ebbb69c46fc0c
KhemkaAkshat <144114211+KhemkaAkshat@users.noreply.github.com>
2024-08-28 23:49:35 +0530

    Created navbar for the home page of the website

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,26 +6,25 @@ import { IoSearch } from "react-icons/io5";
 function Home() {
   return (
     <>
-      <div className=" pt-5 px-8 flex items-center bg-contain bg-center">
+      <div className="pt-4 px-6 flex justify-between items-center  ">
         {/* Navbar */}
-        <div className=" mr-[30vw]">
-          <img className="w-[14vw]" src={logo} alt="Logo" />
+        <div>
+          <img className="w-[10vw]" src={logo} alt="Logo" />
         </div>
-        <div className=" ml-[24vw] flex gap-20 font-semibold text-xl">
-          <button>Explore</button>
-          <button>Lists</button>
-          <button>Profile</button>
+        <div className="flex">
+          <input
+            type="text"
+            className="text-md border border-gray-300 p-2 w-[30vw] rounded-l-md focus:outline-none focus:border-purple-400"
+            placeholder="Search what you need"
+          />
+          <button className="p-2 bg-purple-400 text-white rounded-r-md ">
+            <IoSearch className="text-xl w-[3vw] hover:scale-125 transition-all ease" />
+          </button>
         </div>
-      </div>
-      <div className=" justify-center items-center w-1/2 p-20">
-        <div className="font-bold text-3xl ">
-            Find the Perfect Freelancer for you
-        </div>
-        <div className="flex mt-10 ">
-            <input type="text" className="text-xl border-2 p-5 w-3/4 rounded-xl" />
-            <button className="ml-[-1vw] p-5 bg-purple-400 rounded-tr-xl rounded-br-xl ">
-            <IoSearch className="text-3xl"/>
-            </button>
+        <div className="flex gap-8 font-medium text-lg text-gray-700">
+          <button className="hover:text-purple-400 transition-colors">Explore</button>
+          <button className="hover:text-purple-400 transition-colors">Orders</button>
+          <button className="hover:text-purple-400 transition-colors">Profile</button>
         </div>
       </div>
     </>
